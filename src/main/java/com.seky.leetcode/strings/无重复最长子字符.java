53b69829b3e285c874558b159f22266601d7d421
wangfei03 <Seky6050>
2020-09-15 14:39:09 +0800

    无重复最长子字符

--- src/main/java/com.seky.leetcode/strings/无重复最长子字符.java
+++ src/main/java/com.seky.leetcode/strings/无重复最长子字符.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 /**
  * @author: wangfei
  * @create: 2020/9/14 11:14
- * @description: 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * @description: 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度 (移动窗口问题)
  */
 public class 无重复最长子字符 {
 
